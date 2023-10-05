a62f382b364f804e37dadd874073aef9647b4d3a
allobang <76904107+allobang@users.noreply.github.com>
2023-10-05 16:03:18 +0800

    Initial

---
+++ script.js
@@ -0,0 +1,182 @@
+const gradesData = {
+    "grades": [
+        {
+            "grade": 2,
+            "questions": [
+                {
+                    "difficulty": "easy",
+                    "items": [
+                        {"question": "5 - 3", "choices": ["1", "2", "3", "4"], "answer": "2"},
+                        {"question": "2/6 & 4/6", "choices": ["<", ">", "="], "answer": "<"},
+                        {"question": "1cm & 1m", "choices": ["<", ">", "="], "answer": "<"},
+                        {"question": "9 - 0", "choices": ["7", "8", "9", "10"], "answer": "9"},
+                        {"question": "1/3 & 2/3", "choices": ["<", ">", "="], "answer": "<"},
+                        {"question": "1m & 100cm", "choices": ["<", ">", "="], "answer": "="},
+                        {"question": "22 - 12", "choices": ["8", "10", "12", "14"], "answer": "10"},
+                        {"question": "5/8 & 3/8", "choices": ["<", ">", "="], "answer": ">"},
+                        {"question": "5cm & 2m", "choices": ["<", ">", "="], "answer": "<"},
+                        {"question": "8 - 5", "choices": ["2", "3", "4", "5"], "answer": "3"},
+                        {"question": "1/4 & 1/4", "choices": ["<", ">", "="], "answer": "="},
+                        {"question": "6m & 25cm", "choices": ["<", ">", "="], "answer": ">"},
+                        {"question": "10 - 3", "choices": ["5", "6", "7", "8"], "answer": "7"},
+                        {"question": "11/12 & 5/12", "choices": ["<", ">", "="], "answer": ">"},
+                        {"question": "2m & 30cm", "choices": ["<", ">", "="], "answer": ">"},
+                        {"question": "15 - 10", "choices": ["3", "4", "5", "6"], "answer": "5"},
+                        {"question": "2/5 & 4/5", "choices": ["<", ">", "="], "answer": "<"},
+                        {"question": "45cm & 36m", "choices": ["<", ">", "="], "answer": "<"},
+                        {"question": "17 - 17", "choices": ["-1", "0", "1", "2"], "answer": "0"},
+                        {"question": "7/10 & 3/10", "choices": ["<", ">", "="], "answer": ">"}
+                    ]
+                }      ,
+                {
+                    "difficulty": "medium",
+                    "items": [
+                        {"question": "55 - 8", "choices": ["46", "47", "48", "49"], "answer": "47"},
+                        {"question": "6/12 & 2/12", "choices": ["<", ">", "="], "answer": ">"},
+                        {"question": "29cm & 25cm", "choices": ["<", ">", "="], "answer": ">"},
+                        {"question": "48 - 15", "choices": ["32", "33", "34", "35"], "answer": "33"},
+                        {"question": "5/15 & 10/15", "choices": ["<", ">", "="], "answer": "<"},
+                        {"question": "3m & 3cm", "choices": ["<", ">", "="], "answer": ">"},
+                        {"question": "35 - 10", "choices": ["24", "25", "26", "27"], "answer": "25"},
+                        {"question": "1/10 & 3/10", "choices": ["<", ">", "="], "answer": "<"},
+                        {"question": "300cm & 3m", "choices": ["<", ">", "="], "answer": "="},
+                        {"question": "88 - 25", "choices": ["61", "62", "63", "64"], "answer": "63"},
+                        {"question": "4/12 & 8/12", "choices": ["<", ">", "="], "answer": "<"},
+                        {"question": "7m & 7cm", "choices": ["<", ">", "="], "answer": ">"},
+                        {"question": "73 - 35", "choices": ["37", "38", "39", "40"], "answer": "38"},
+                        {"question": "3/8 & 1/8", "choices": ["<", ">", "="], "answer": ">"},
+                        {"question": "300cm & 30m", "choices": ["<", ">", "="], "answer": "<"},
+                        {"question": "25 - 5", "choices": ["19", "20", "21", "22"], "answer": "20"},
+                        {"question": "4/16 & 8/16", "choices": ["<", ">", "="], "answer": "<"},
+                        {"question": "2m & 350cm", "choices": ["<", ">", "="], "answer": "<"},
+                        {"question": "66 - 26", "choices": ["39", "40", "41", "42"], "answer": "40"},
+                        {"question": "7/18 & 7/18", "choices": ["<", ">", "="], "answer": "="}
+                    ]
+                },
+                {
+                    "difficulty": "hard",
+                    "items": [
+                        {"question": "157 - 58", "choices": ["98", "99", "100", "101"], "answer": "99"},
+                        {"question": "7/19 & 6/19", "choices": ["<", ">", "="], "answer": ">"},
+                        {"question": ".5m & 25cm", "choices": ["<", ">", "="], "answer": ">"},
+                        {"question": "551 - 370", "choices": ["180", "181", "182", "183"], "answer": "181"},
+                        {"question": "13/25 & 5/25", "choices": ["<", ">", "="], "answer": ">"},
+                        {"question": ".3m & 3m", "choices": ["<", ">", "="], "answer": "<"},
+                        {"question": "661 - 554", "choices": ["106", "107", "108", "109"], "answer": "107"},
+                        {"question": "9/22 & 9/22", "choices": ["<", ">", "="], "answer": "="},
+                        {"question": "400cm & 200m", "choices": ["<", ">", "="], "answer": "<"},
+                        {"question": "240 - 73", "choices": ["166", "167", "168", "169"], "answer": "167"},
+                        {"question": "5/29 & 16/29", "choices": ["<", ">", "="], "answer": "<"},
+                        {"question": "42m & 15cm", "choices": ["<", ">", "="], "answer": ">"},
+                        {"question": "202 - 98", "choices": ["103", "104", "105", "106"], "answer": "104"},
+                        {"question": "7/17 & 9/17", "choices": ["<", ">", "="], "answer": "<"},
+                        {"question": "35cm & 520cm", "choices": ["<", ">", "="], "answer": "<"},
+                        {"question": "326 - 159", "choices": ["166", "167", "168", "169"], "answer": "167"},
+                        {"question": "15/26 & 9/26", "choices": ["<", ">", "="], "answer": ">"},
+                        {"question": ".5m & 50cm", "choices": ["<", ">", "="], "answer": "="},
+                        {"question": "268 - 65", "choices": ["202", "203", "204", "205"], "answer": "203"},
+                        {"question": "250cm & 1.5m", "choices": ["<", ">", "="], "answer": ">"}
+                    ]
+                }
+            ]
+        }
+    ]
+}
+
+;
+
+const gradeSelect = document.getElementById('gradeSelect');
+const difficultySelect = document.getElementById('difficultySelect');
+const startBtn = document.getElementById('startBtn');
+const questionArea = document.getElementById('questionArea');
+const timerElem = document.getElementById('timer');
+const questionText = document.getElementById('questionText');
+const choicesElem = document.getElementById('choices');
+const scoreElem = document.getElementById('score');
+
+let currentQuestionIndex = 0;
+let correctAnswers = 0;
+let timer;
+
+gradesData.grades.forEach(grade => {
+    const option = document.createElement('option');
+    option.value = grade.grade;
+    option.textContent = `Grade ${grade.grade}`;
+    gradeSelect.appendChild(option);
+});
+
+gradeSelect.addEventListener('change', function() {
+    const selectedGrade = this.value;
+    const difficulties = gradesData.grades.find(grade => grade.grade == selectedGrade).questions.map(q => q.difficulty);
+
+    difficultySelect.innerHTML = ''; // Clear previous options
+    difficulties.forEach(difficulty => {
+        const option = document.createElement('option');
+        option.value = difficulty;
+        option.textContent = difficulty;
+        difficultySelect.appendChild(option);
+    });
+});
+
+startBtn.addEventListener('click', startQuiz);
+
+function startQuiz() {
+    console.log("Quiz started!");
+    const selectedGrade = gradeSelect.value;
+    const selectedDifficulty = difficultySelect.value;
+
+    const questions = gradesData.grades
+        .find(grade => grade.grade == selectedGrade)
+        .questions.find(q => q.difficulty == selectedDifficulty).items;
+        console.log(questions);
+    currentQuestionIndex = 0;
+    correctAnswers = 0;
+    displayQuestion(questions);
+
+    timerElem.textContent = 10;
+    timer = setInterval(function() {
+        let currentTime = parseInt(timerElem.textContent);
+        if (currentTime <= 0) {
+            nextQuestion(questions);
+        } else {
+            timerElem.textContent = currentTime - 1;
+        }
+    }, 1000);
+}
+
+function displayQuestion(questions) {
+    console.log("Displaying question:", currentQuestionIndex);
+    const currentQuestion = questions[currentQuestionIndex];
+    console.log(currentQuestion);
+    questionText.textContent = currentQuestion.question;
+    choicesElem.innerHTML = '';
+    currentQuestion.choices.forEach((choice, index) => {
+        const button = document.createElement('button');
+        button.textContent = choice;
+        button.addEventListener('click', function() {
+            if (choice == currentQuestion.answer) correctAnswers++;
+            nextQuestion(questions);
+        });
+        choicesElem.appendChild(button);
+        questionArea.classList.remove('hidden');
+    });
+}
+
+function nextQuestion(questions) {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        timerElem.textContent = 10; // Reset timer
+        displayQuestion(questions);
+    } else {
+        endQuiz();
+    }
+}
+
+function endQuiz() {
+    clearInterval(timer);
+    questionArea.classList.add('hidden');
+    scoreElem.classList.remove('hidden');
+    scoreElem.textContent = `Your score: ${correctAnswers} / ${currentQuestionIndex}`;
+}
+
+gradeSelect.dispatchEvent(new Event('change'));
